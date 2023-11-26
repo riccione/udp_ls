97865a5fc32c797977b07cbae3360223ff14848b
Zero <zero@zero-ms7c56.ignes-zero.com>
2023-11-26 17:08:23 -0500

    Combine clients for UDP and TCP in one fn Clean the code

--- udp_ls/udp_ls.py
+++ udp_ls/udp_ls.py
@@ -2,6 +2,7 @@ import socket
 import concurrent.futures
 import time
 
+
 def udp_server(host, port):
     # Create a UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
@@ -14,32 +15,52 @@ def udp_server(host, port):
             data, addr = sock.recvfrom(1024)
             print(f"Received data from {addr} on port {port}: {data.decode('utf-8')}")
 
-def udp_client(host, port):
-    i = 0
-#    breakpoint()
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while True:
-        msg = f"Hello, UDP Server! {i}"
 
-        s.sendto(msg.encode("utf-8"), (host, port))
+def tcp_server(host, port):
+    # breakpoint()
+    # Create a UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Bind the socket to the specified port
+        sock.bind((host, port))
+        sock.listen(5)  # max number of queued conns
+        print(f"Listening on TCP port {port}...")
+
+        while True:
+            # Receive data from the socket
+            conn, addr = sock.accept()
+            print(f"Connection established from {addr}")
+            data = conn.recv(1024)
+            print(f"Received data: {data.decode('utf-8')}")
+
+
+def client(protocol, host, port):
+    i = 0
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if protocol == "UDP":
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = f"Hello, UDP Server! {i}"
+        if protocol == "UDP":
+            s.sendto(msg.encode("utf-8"), (host, port))
+        else:
+            s.connect((host, port))
+            s.sendall(msg.encode("utf-8"))
         time.sleep(2)
         i += 1
+        s.close()
 
-    s.close()
 
 def main():
-    host = "0.0.0.0"
+    host = "127.0.0.1"
     port1 = 10001
     port2 = 10002
     port_client = 8080
-
-    # Create a ThreadPoolExecutor with two threads
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         # Submit each listener to the executor
-        executor.submit(udp_server, host, port1)
-        executor.submit(udp_server, host, port2)
-        executor.submit(udp_client, host, port_client)
-    # The ThreadPoolExecutor will manage the threads and automatically clean up when done
+        executor.submit(client, "TCP", host, 8080)
+        executor.submit(tcp_server, host, 10001)
+        executor.submit(tcp_server, host, 10002)
+        # The ThreadPoolExecutor will manage the threads and automatically clean up when done
 
 
 if __name__ == "__main__":
